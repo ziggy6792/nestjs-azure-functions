0c0e73e5b057bd33143f2fa6fbca3840242b869d
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-17 01:11:43 +0700

    upgrade packages

--- main/index.ts
+++ main/index.ts
@@ -1,7 +1,15 @@
-import { Context, HttpRequest } from '@azure/functions';
-import { AzureHttpAdapter } from '@nestjs/azure-func-http';
+import { InvocationContext, HttpRequest } from '@azure/functions';
 import { createApp } from '../src/main.azure';
+import serverlessExpress from '@codegenie/serverless-express';
 
-export default function(context: Context, req: HttpRequest): void {
-  AzureHttpAdapter.handle(createApp, context, req);
+let cachedServer: any;
+
+export default async function (
+  context: InvocationContext,
+  req: HttpRequest,
+): Promise<void> {
+  if (!cachedServer) {
+    cachedServer = serverlessExpress({ app: await createApp() });
+  }
+  return cachedServer(context, req);
 }
